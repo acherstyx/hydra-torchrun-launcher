0c2116c3b4e92f5092e15b9f5f88878519595009
acherstyx <acherstyx@gmail.com>
2024-05-18 17:59:51 +0800

    Add test cases, update to support torch==2.3.0

--- hydra_plugins/hydra_torchrun_launcher/config.py
+++ hydra_plugins/hydra_torchrun_launcher/config.py
@@ -3,25 +3,31 @@
 # @Author  : Yaojie Shen
 # @Project : hydra-torchrun-launcher
 # @File    : config.py
-
 from dataclasses import dataclass, field
-from hydra.core.config_store import ConfigStore
 from typing import Optional, List
+
+from packaging.version import Version
+from importlib.metadata import version
+
+from hydra.core.config_store import ConfigStore
 
 
 @dataclass
 class TorchDistributedLauncherConf:
     _target_: str = "hydra_plugins.hydra_torchrun_launcher.distributed_launcher.TorchDistributedLauncher"
 
+    # Worker/node size related arguments.
     nnodes: str = "1:1"
     nproc_per_node: str = "1"
 
+    # Rendezvous related arguments
     rdzv_backend: str = "static"
     rdzv_endpoint: str = ""
     rdzv_id: str = "none"
     rdzv_conf: str = ""
     standalone: bool = False
 
+    # User-code launch related arguments.
     max_restarts: int = 0
     monitor_interval: int = 5
     start_method: str = "spawn"
@@ -33,15 +39,23 @@ class TorchDistributedLauncherConf:
     redirects: str = "0"
     tee: str = "0"
 
+    # Backwards compatible parameters with caffe2.distributed.launch.
     node_rank: int = 0
     master_addr: str = "127.0.0.1"
     master_port: int = 29500
     local_addr: Optional[str] = None
 
+    # Positional arguments.
     # Should not set
     training_script: str = ""
     training_script_args: List[str] = field(default_factory=list)
 
+
+if Version(version("torch")) >= Version("2.3"):
+    @dataclass
+    class TorchDistributedLauncherConf(TorchDistributedLauncherConf):
+        logs_specs: Optional[str] = None
+        local_ranks_filter: str = ""
 
 ConfigStore.instance().store(
     group="hydra/launcher", name="torchrun", node=TorchDistributedLauncherConf
---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 5/15/24
+# @Author  : Yaojie Shen
+# @Project : hydra-torchrun-launcher
+# @File    : __init__.py
